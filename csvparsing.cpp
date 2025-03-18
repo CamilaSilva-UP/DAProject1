8946303834821ea925ea4d1e86de9b3659b03264
Camila Silva <up202305458@up.pt>
2025-03-18 11:21:34 +0000

    Created csvparsing.cpp

---
+++ csvparsing.cpp
@@ -0,0 +1,6 @@
+//this function should receive a .csv file, and parse it
+//for testing purposes, make it print the values, DON'T CREATE A GRAPH UNTIL IT IS TESTED PROPERLY
+
+void csvParsing() {
+
+}
