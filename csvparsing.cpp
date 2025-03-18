67d4a9b6a58bed9199a2610378db12fd21e15a16
Camila Silva <up202305458@up.pt>
2025-03-18 11:31:34 +0000

    working on csvparsing

--- csvparsing.cpp
+++ csvparsing.cpp
@@ -1,6 +1,15 @@
 //this function should receive a .csv file, and parse it
 //for testing purposes, make it print the values, DON'T CREATE A GRAPH UNTIL IT IS TESTED PROPERLY
 
-void csvParsing() {
+#include <string>
+#include <fstream>
+#include <iostream>
+
+void csvParsing(const std::string& filepath) {
+    std::ifstream file(filepath);
+    if (!file) {
+        std::cerr << "Error opening file " << filepath << std::endl;
+    }
+
 
 }
