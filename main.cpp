3d2f643e5c42be2450e34b9de50801b74ee6ab2c
Camila <up202305458@up.pt>
2025-03-22 10:42:39 +0000

    Distances parsing also being tested in main

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 
 int main() {
     std::cout << "Testing Graph functionality" << std::endl;
+    std::cout << std::endl;
     csvLocationParsing("csv_examples/Locations.csv");
+    std::cout << std::endl;
+    csvDistancesParsing("csv_examples/Distances.csv");
     return  0;
 }
