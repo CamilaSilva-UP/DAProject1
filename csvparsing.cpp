91869a4711e61bcf1e7e4399c705607268f4f2a0
Camila <up202305458@up.pt>
2025-03-22 10:40:20 +0000

    Rewrote split function in a more natural way. Cleaned up parsing code

--- csvparsing.cpp
+++ csvparsing.cpp
@@ -11,17 +11,21 @@
 
 //for testing purposes, make it print the values, DON'T CREATE A GRAPH UNTIL IT IS TESTED PROPERLY
 //use files in csv_examples for testing
-std:: vector<std::string> split(const std::string& line, char delim) {
-    std:: vector<std::string> tokens;
-    std::string token;
-    std::istringstream tokenStream(line);
-    while (std::getline(tokenStream, token, delim)) {
-        token.erase(0, token.find_first_not_of(delim));
-        token.erase(token.find_last_not_of(delim) + 1);
-        tokens.push_back(token);
+
+std::vector<std::string> split(std::string& s, const char& delimiter) {
+    std::vector<std::string> values;
+    size_t pos = 0;
+    std::string value;
+    while ((pos = s.find(delimiter)) != std::string::npos) {
+        value = s.substr(0, pos);
+        values.push_back(value);
+        s.erase(0, pos + 1);
     }
-    return tokens;
+    values.push_back(s);
+
+    return values;
 }
+
 //Parses Locations.csv and creates the vertices
 void csvLocationParsing(const std::string& filepath) {
     std::ifstream input(filepath);
@@ -30,20 +34,12 @@ void csvLocationParsing(const std::string& filepath) {
         return;
     }
 
-    //for (std::string line; std::getline(input,line);){
-      //std::cout << line <<std::endl;
-      //divides the line in tokens separed by ','
-      //std::istringstream iss(line);
-      //for(std::string token; std::getline(iss,token, ',');){
-        //std::cout << token << ' | ';
-      //}
-      //std::cout << std::endl;
-    //}
     std::string line;
+    std::getline(input, line);  // first row is not needed
     while (std::getline(input, line)) {
-        std::vector<std::string> tokens = split(line, ',');
-        for (const auto& token : tokens) {
-            std::cout << token << '|';
+        std::vector<std::string> values = split(line, ',');
+        for (const auto& value : values) {
+            std::cout << value << '|';
         }
         std::cout << std::endl;
     }
@@ -56,19 +52,13 @@ void csvDistancesParsing(const std::string& filepath){
         std::cerr << "Error opening file " << filepath << std::endl;
         return;
     }
+
     std::string line;
     std::getline(input, line);
-    //for(std::string line; std::getline(input,line);){
-     // std::cout << line <<std::endl;
-      //std::istringstream iss(line);
-      //for(std::string token; std::getline(iss,token, ',');){
-      //  std::cout << token << ' | ';
-      //}
-      //std::cout << std::endl;
     while (std::getline(input, line)) {
-        std::vector<std::string> tokens = split(line, ',');
-        for (const auto& token : tokens) {
-            std::cout << token << '|';
+        std::vector<std::string> values = split(line, ',');
+        for (const auto& value : values) {
+            std::cout << value << '|';
         }
         std::cout << std::endl;
     }
