8f7aafa7eeab86d1ce4f09cf58ad28f6f52567c6
Camila <up202305458@up.pt>
2025-03-27 13:17:50 +0000

    Added batch mode documentation

--- batchMode.h
+++ batchMode.h
@@ -4,5 +4,16 @@
 
 #ifndef BATCHMODE_H
 #define BATCHMODE_H
+
+///
+///@file batchMode.h
+///@brief Header file containing the declaration of the function required for batch mode
+
+///
+///@brief Runs batch mode
+///
+///@details Processes the input.txt file in the Batch_Mode_Files directory,
+///and creates an output.txt file with the corresponding output routes
+///
 void runBatchMode();
 #endif //BATCHMODE_H
